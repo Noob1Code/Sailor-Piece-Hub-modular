29ea8852ba27792a8b7b08f86dd207e1ca3e7a7e
Noob1Code <137302806+Noob1Code@users.noreply.github.com>
2026-03-28 21:34:31 -0300

    Update RandomService.lua

--- Sailor Piece Modular/Services/RandomService.lua
+++ Sailor Piece Modular/Services/RandomService.lua
@@ -1,1 +1,21 @@
 
+-- ========================================================================
+-- 🎲 SERVIÇO: RANDOM SERVICE (SISTEMA DE HUMANIZAÇÃO ANTI-CHEAT)
+-- ========================================================================
+local RandomService = {}
+
+function RandomService:GetTime(min, max)
+    if not max then 
+        max = min 
+        min = 0 
+    end
+    return min + (math.random() * (max - min))
+end
+
+function RandomService:Wait(min, max)
+    local waitTime = self:GetTime(min, max)
+    task.wait(waitTime)
+    return waitTime
+end
+
+return RandomService
